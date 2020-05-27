bdbec4252e0ce7f7f6f8a08e5fb4d8441abe89c1
Jularin <58310353+Jularin@users.noreply.github.com>
2020-05-27 12:45:15 +0300

    V 1.2.1
    Small bug fixes. Function descripsions. updated todo list.

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from datetime import datetime
 bot = telebot.TeleBot('1088060110:AAF-s9TPSbMAhpOXFVUPj50S0Ya8hh38jAc')
 types = telebot.types
 
+# TO DO!! make funcs: admin keyboard and users keyboard, main menu, add categories for items(2 or 3) add multiple buying
 
-# TO DO!! make funcs: admin kayboard and users keyboard
+
+# Finish this func
+def sure(message):
+    bot.send_message(message.chat.id, "Are you sure? y/n")
+
 
 def logging(message):
+    """Logging: write all commands in txt file"""
     info = "{}  Name: {} {} User id: {} message text: {}".format(str(datetime.now().strftime("%d-%m-%Y %H:%M")),
                                                                  str(message.from_user.first_name),
                                                                  str(message.from_user.last_name),
@@ -25,34 +31,36 @@ def logging(message):
 
 
 def keyboard(flag):
+    """Function which generate keyboard"""
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     buy_button = types.KeyboardButton(text='/buy Купить товар')
-    check_balance_button = types.KeyboardButton(text='/balance нажмите сюда, чтобы узнать свой баланс')
-    help_button = types.KeyboardButton(text='/help  нажмите сюда, чтобы узнать о боте')
+    check_balance_button = types.KeyboardButton(text='/balance Баланс')
+    help_button = types.KeyboardButton(text='/help  Помощь')
     keyboard.add(buy_button, check_balance_button, help_button)
     if flag:
-        keyboard.add(types.KeyboardButton('/start - назад'))
+        keyboard.add(types.KeyboardButton('/start Главное меню'))
     return keyboard
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    """Main menu"""
     logging(message)
     work_with_db.check_in_db(message)
     bot.send_message(message.chat.id,
-                     "You're using new bot! 'DESCRIPTION'\n/dump\n/balance\n/adduser\n/changebalance\n/adduser\n/help",
+                     "You're using new bot! 'DESCRIPTION'\n/dump\n/balance\n/adduser\n/changebalance\n/adduser\n/help",  # here description of bot
                      reply_markup=keyboard(0))
 
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
     logging(message)
-    global keyboard
-    bot.send_message(message.chat.id, 'bot description', reply_markup=keyboard(1))
+    bot.send_message(message.chat.id, 'Coded by M K (@topkekl)', reply_markup=keyboard(1))
 
 
 @bot.message_handler(commands=['buy'])
 def buy_bot(message):
+    """Buying items"""
     logging(message)
     try:
         with open('items.txt', encoding='utf-8') as items:
@@ -77,6 +85,7 @@ def buy_bot(message):
 
 @bot.message_handler(commands=['balance'])
 def check_balance(message):
+    """Send balance to user"""
     logging(message)
     work_with_db.check_in_db(message)
     bot.send_message(message.chat.id,
@@ -85,6 +94,7 @@ def check_balance(message):
 
 @bot.message_handler(commands=['admin'])
 def admin(message):
+    """Admin panel"""
     logging(message)
     if work_with_db.find_and_return_value(str(message.chat.id), 3) == 'admin':
         keyboard = types.ReplyKeyboardMarkup(row_width=2)
@@ -102,6 +112,7 @@ def admin(message):
 
 @bot.message_handler(commands=['dump'])
 def dump(message):
+    """Send dump of db for admin"""
     logging(message)
     work_with_db.check_in_db(message)
     if work_with_db.find_and_return_value(str(message.chat.id), 3) == 'admin':  # if command called by admin
@@ -113,17 +124,21 @@ def dump(message):
 
 
 def add_user(message):
+    """Next step for add_user_bot function"""
     logging(message)
     bot.send_message(message.chat.id, work_with_db.add_user(str(message.text)))
 
 
 @bot.message_handler(commands=['adduser'])
 def add_user_bot(message):
+    """Add new user by admin"""
     logging(message)
-    bot.register_next_step_handler(bot.send_message(message.chat.id, "Укажите айди"), add_user)
+    if work_with_db.find_and_return_value(str(message.chat.id), 3) == 'admin':  # if command called by admin
+        bot.register_next_step_handler(bot.send_message(message.chat.id, "Укажите айди"), add_user)
 
 
 def change_balance(message):
+    """Change balance by admin"""
     logging(message)
     print(message.text)
     try:
@@ -142,21 +157,26 @@ def change_balance(message):
 
 @bot.message_handler(commands=['changebalance'])
 def change_balance_bot(message):
+    """Check who want change balance and get id and amount for change"""
     logging(message)
-    bot.register_next_step_handler(
-        bot.send_message(message.chat.id, "Укажите какой айди и на какую сумму изменить (через пробел)"),
-        change_balance)
+    if work_with_db.find_and_return_value(str(message.chat.id), 3) == 'admin':  # if command called by admin
+        bot.register_next_step_handler(
+            bot.send_message(message.chat.id, "Укажите какой айди и на какую сумму изменить (через пробел)"),
+            change_balance)
 
 
 @bot.message_handler(commands=['drop'])
 def drop_table(message):
+    """Drop table by admin"""
     logging(message)
     work_with_db.check_in_db(message)
-    if work_with_db.find_and_return_value(str(message.chat.id), 3) == 'admin':
-        bot.send_message(message.chat.id, work_with_db.drop_table())
+    if work_with_db.find_and_return_value(str(message.chat.id), 3) == 'admin':  # if command called by admin
+        if work_with_db.find_and_return_value(str(message.chat.id), 3) == 'admin':
+            bot.send_message(message.chat.id, work_with_db.drop_table())
 
 
 def mailing(message):
+    """Mailing by users in database"""
     logging(message)
     database = work_with_db.dump()
     for tuples in database:
@@ -169,8 +189,10 @@ def mailing(message):
 
 @bot.message_handler(commands=['mailing'])
 def mailing_bot(message):
+    """Function get mailing text"""
     logging(message)
-    bot.register_next_step_handler(bot.send_message(message.chat.id, "Send me text to mailing"), mailing)
+    if work_with_db.find_and_return_value(str(message.chat.id), 3) == 'admin':  # if command called by admin
+        bot.register_next_step_handler(bot.send_message(message.chat.id, "Send me text to mailing"), mailing)
 
 
 # ADD NEW COMMANDS ONLY ABOVE THIS FUNC. New commands don't read bc all will be 'content_types = text'
